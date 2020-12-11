f03c8a25e2bfcea830d43a153a4990056c3eb2ac
koolearn <bikeyou@xdfzx.com>
2020-12-11 15:14:31 +0800

    添加多入口

---
+++ src/demo1/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  version: '1.0.0',
+  title: 'title',
+  // config: {
+  //   template: ''
+  // }
+}
--- vue.config.js
+++ vue.config.js
@@ -9,18 +9,21 @@ const { parseArgv, testFile } = require('./utils.js');
 // let projectConfig = require('./src/demo/config.js');  // 不加 ./ 会报错
 // console.log('projectConfig', projectConfig);
 
-console.log(process.argv);
+// console.log(process.argv);
 
 const isBuild = process.argv[2] == 'build'
 let argvData = parseArgv(process.argv);
 let project = argvData('--project') || 'demo';
+
+// console.log('project', project);
+
 let testProjectFile = testFile(project);
 
 const zip = argvData('--zip');
 
-console.log('zip', zip);
+// console.log('zip', zip);
 
-// console.log('testFile', testProjectFile('config.js'));
+console.log('testFile', testProjectFile('config.js'));
 // console.log('testFile', JSON.parse(testProjectFile('config.js')).version);
 
 if (!testProjectFile('main.js')) {
@@ -42,21 +45,31 @@ if (!projectConfig.version) {
 
 // console.log('====', fs.existsSync(path.join(__dirname, './dis/t'))(`${project}-${projectConfig.version}`))
 
-if (fs.existsSync(path.join(__dirname, `./dist/${project}-${projectConfig.version}`)) && isBuild) {
-  console.log(chalk.red.bgCyan.bold('--------项目缺少配置文件---------\n'));
-  process.exit();
-}
+// if (fs.existsSync(path.join(__dirname, `./dist/${project}-${projectConfig.version}`)) && isBuild) {
+//   console.log(chalk.red.bgCyan.bold('--------项目缺少配置文件---------\n'));
+//   process.exit();
+// }
+
+let entryName = projectConfig.filename || 'index';
 
 
 const config = {
   // outputDir: `dist/${project}-${project.version}`,
-  outputDir: `dist/${project}-${projectConfig.version}`,
+  // outputDir: `dist/${project}-${projectConfig.version}`,
   pages: {
-    [project]: {
+    // index: {
+    [entryName]: {
+    // 1: {
       entry: `./src/${project}/main.js`,
       template: 'public/index.html',
       title: projectConfig.title || project,
-      filename: projectConfig.filename || 'index.html'
+      filename: entryName + '.html'
+    },
+    demo1: {
+      entry: `./src/demo1/main.js`,
+      template: 'public/index.html',
+      title: '111',
+      filename: 'demo1.html'
     }
   },
   chainWebpack: (config) => {
